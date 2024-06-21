f7efd2891e8775017c0c1e0e411f2c78e801e324
Scott <ser.arroz@gmail.com>
2024-06-21 14:52:09 -0600

    func main, func parseShortcuts

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func main () {
     fmt.Println("Collecting Key Bindings from KDE")
@@ -12,5 +17,27 @@ func main () {
     if err != nil {
         fmt.Printf("Error parsing shortcuts: %v\n", err)
         return
+    }
+
+    err = saveShortcutsToFIle(shortcuts, outputFilePath)
+    if err != nil {
+        fmt.Printf("Error Saving shortcuts to file: %v\n", err)
+        return
+    }
+}
+
+func parseShortcuts(filepath string, sourceName string) ([][3]string, error) {
+    content, err := os.ReadFile(filepath)
+    if err != nil {
+        return nil, err
+    }
+
+    lines := strings.Split(string(content), "\n")
+    var shortcuts [][3]string
+
+    for _, line := range lines {
+        if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
+            section := strings.TrimSuffix(strings.TrimPrefix(line, "["), "]"),
+
     }
 }
